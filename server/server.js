df37b6ab6fc7f1c184cc1c04b5ca1df19439b808
Ashrith Rao <raoashrith16601@gmail.com>
2026-09-08 10:27:01 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ app.use(express.json());
 
 app.use("/api", tripRoutes);
 
-app.listen(5000, () => {
-    console.log("Listening on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
